0b0339148fbbd0b4e39b9b7aa9d13fb8168e30fe
Alejandro Granadillo <slayerfat@gmail.com>
2016-10-31 17:36:08 -0400

    test: updated travis file and added few more tests

--- test.js
+++ test.js
@@ -6,6 +6,26 @@ var describe    = require('mocha').describe;
 var tsNamespace = require('ts-namespaces');
 var tsModule    = require('ts-modules');
 
+describe('ts-namespaces', function () {
+    it('should not be undefined', function () {
+        expect(tsNamespace).to.not.equal(undefined);
+    });
+
+    it('core should not be undefined', function () {
+        expect(tsNamespace.Core).to.not.equal(undefined);
+    });
+});
+
+describe('ts-modules', function () {
+    it('should not be undefined', function () {
+        expect(tsModule).to.not.equal(undefined);
+    });
+
+    it('core should not be undefined', function () {
+        expect(tsModule.AnotherCore).to.not.equal(undefined);
+    });
+});
+
 var namespaceService = new tsNamespace.Core.Service();
 var moduleService    = new tsModule.AnotherCore.Service();
 
